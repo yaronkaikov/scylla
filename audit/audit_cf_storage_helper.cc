yaronkaikov/scylla

commit f303665231241700e0e71849c09991590ef0d747

audit: show real value in `error` column for direct queries
`error` column in audit log used to show almost always `False` because what it
was checking was only the access rights - not the real success of the
operation. This commit fixes the issue by logging also the success state of
`process_authorized_statement()`.

On this occasion I also unified the ordering of columns.

Fixes #572

Closes #2914

audit/audit_cf_storage_helper.cc
@@ -38,12 +38,12 @@ audit_cf_storage_helper::audit_cf_storage_helper(cql3::query_processor& qp)
                        "event_time,"
                        "category,"
                        "consistency,"
-                       "table_name,"
+                       "error,"
                        "keyspace_name,"
                        "operation,"
                        "source,"
-                       "username,"
-                       "error) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                       "table_name,"
+                       "username) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
                        KEYSPACE_NAME, TABLE_NAME))
     , _dummy_query_state(service::client_state::for_internal_calls(), empty_service_permit())
 {
@@ -91,12 +91,12 @@ cql3::query_options audit_cf_storage_helper::make_data(const audit_info* audit_i
         cql3::raw_value::make_value(uuid_type->decompose(time_id)),
         cql3::raw_value::make_value(utf8_type->decompose(audit_info->category_string())),
         cql3::raw_value::make_value(utf8_type->decompose(sstring(consistency_level.str()))),
-        cql3::raw_value::make_value(utf8_type->decompose(audit_info->table())),
+        cql3::raw_value::make_value(boolean_type->decompose(error)),
         cql3::raw_value::make_value(utf8_type->decompose(audit_info->keyspace())),
         cql3::raw_value::make_value(utf8_type->decompose(audit_info->query())),
         cql3::raw_value::make_value(inet_addr_type->decompose(client_ip.addr())),
+        cql3::raw_value::make_value(utf8_type->decompose(audit_info->table())),
         cql3::raw_value::make_value(utf8_type->decompose(username)),
-        cql3::raw_value::make_value(boolean_type->decompose(error)),
     };
     return cql3::query_options(cql3::default_cql_config, db::consistency_level::ONE, std::nullopt, std::move(values), false, cql3::query_options::specific_options::DEFAULT);
 }

cql3/query_processor.cc
@@ -499,19 +499,34 @@ query_processor::execute_direct_without_checking_exception_message(const sstring
 #endif
     tracing::trace(query_state.get_trace_state(), "Processing a statement");
     return cql_statement->check_access(*this, query_state.get_client_state()).then_wrapped([this, cql_statement, warnings = std::move(warnings), &query_state, &options] (auto&& access_future) mutable {
-      bool failed = access_future.failed();
-      return audit::inspect(cql_statement, query_state, options, failed).then([this, cql_statement, warnings = std::move(warnings), &query_state, &options, access_future = std::move(access_future)] () mutable {
         if (access_future.failed()) {
-            std::rethrow_exception(access_future.get_exception());
+            return audit::inspect(cql_statement, query_state, options, true).then([cql_statement, access_future = std::move(access_future)] () mutable -> ::shared_ptr<result_message> {
+                std::rethrow_exception(access_future.get_exception());
+            });
         }
-        return process_authorized_statement(std::move(cql_statement), query_state, options).then(
-                [warnings = std::move(warnings)] (::shared_ptr<result_message> m) {
-                    for (const auto& w : warnings) {
-                        m->add_warning(w);
-                    }
-                    return make_ready_future<::shared_ptr<result_message>>(m);
+        return process_authorized_statement(cql_statement, query_state, options).then_wrapped(
+                [cql_statement, warnings = std::move(warnings), &query_state, &options] (future<::shared_ptr<result_message>> mfut) {
+            ::shared_ptr<result_message> m;
+            if (!mfut.failed()) {
+                m = mfut.get0();
+            } else {
+                return audit::inspect(cql_statement, query_state, options, true).then([cql_statement, mfut = std::move(mfut)] () mutable -> ::shared_ptr<result_message> {
+                    std::rethrow_exception(mfut.get_exception());
                 });
-      });
+            }
+
+            bool is_error = true;
+            if (m.get()) {
+                is_error = m->is_exception();
+                for (const auto& w : warnings) {
+                    m->add_warning(w);
+                }
+            }
+
+            return audit::inspect(cql_statement, query_state, options, is_error).then([cql_statement, m] {
+                return make_ready_future<::shared_ptr<result_message>>(m);
+            });
+        });
     });
 }
 

commit b6254d0228ce38803b5e7ce7be8e66008798be99

audit: fix partial mixup of fields in audit table update
A previous commit f3036652312 introduced a correct error logging
in audit, however, some mixup of the logging fields sneaked in.
There are more than one functions that logs audit information and uses
the same prepared statement. However, this commit only touched one of
them and in addition changed the field order in the prepared statement
but didn't change all of the places that use this prepared statement.
This commit sets the fields in the prepared statement back to their
original place and undo the change in the update function.

Ref #2998
Fixes #3109

Tests: unit tests
       audit dtests

This will specifically make the audit_test.TestCQLAudit.test_login dtest pass.

Signed-off-by: Eliran Sinvani <eliransin@scylladb.com>

Closes #3081

audit/audit_cf_storage_helper.cc
@@ -40,12 +40,12 @@ audit_cf_storage_helper::audit_cf_storage_helper(cql3::query_processor& qp, serv
                        "event_time,"
                        "category,"
                        "consistency,"
-                       "error,"
+                       "table_name,"
                        "keyspace_name,"
                        "operation,"
                        "source,"
-                       "table_name,"
-                       "username) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                       "username,"
+                       "error) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
                        KEYSPACE_NAME, TABLE_NAME))
     , _dummy_query_state(service::client_state::for_internal_calls(), empty_service_permit())
 {
@@ -95,12 +95,12 @@ cql3::query_options audit_cf_storage_helper::make_data(const audit_info* audit_i
         cql3::raw_value::make_value(uuid_type->decompose(time_id)),
         cql3::raw_value::make_value(utf8_type->decompose(audit_info->category_string())),
         cql3::raw_value::make_value(utf8_type->decompose(sstring(consistency_level.str()))),
-        cql3::raw_value::make_value(boolean_type->decompose(error)),
+        cql3::raw_value::make_value(utf8_type->decompose(audit_info->table())),
         cql3::raw_value::make_value(utf8_type->decompose(audit_info->keyspace())),
         cql3::raw_value::make_value(utf8_type->decompose(audit_info->query())),
         cql3::raw_value::make_value(inet_addr_type->decompose(client_ip.addr())),
-        cql3::raw_value::make_value(utf8_type->decompose(audit_info->table())),
         cql3::raw_value::make_value(utf8_type->decompose(username)),
+        cql3::raw_value::make_value(boolean_type->decompose(error)),
     };
     return cql3::query_options(cql3::default_cql_config, db::consistency_level::ONE, std::nullopt, std::move(values), false, cql3::query_options::specific_options::DEFAULT);
 }