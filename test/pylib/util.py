scylladb/scylladb

test/pylib/utils: wait_for_cql_and_get_hosts(): sort hosts
Such that a given index in the return hosts refers to the same
underlying Scylla instance, as the same index in the passed-in nodes
list. This is what users of this method intuitively expect, but
currently the returned hosts list is unordered (has random order).

test/pylib/util.py
@@ -115,6 +115,12 @@ async def wait_for_cql_and_get_hosts(cql: Session, servers: list[ServerInfo], de
 
     # Take only hosts from `ip_set` (there may be more)
     hosts = [h for h in hosts if h.address in ip_set]
+
+    # Make sure `hosts` has same order as `servers`, that is: a given index will
+    # refer to the same underlying Scylla instance in both `servers` and `hosts`.
+    servers_by_ip = {s.ip_addr: i for i, s in enumerate(servers)}
+    hosts.sort(key=lambda x: servers_by_ip[x.address])
+
     await asyncio.gather(*(wait_for_cql(cql, h, deadline) for h in hosts))
 
     return hosts

yaronkaikov/scylla

(cherry picked from commit e5afd9b5fbd98d04bcd262324865f78e8ef1724a)

test/pylib/util.py
@@ -79,6 +79,12 @@ async def wait_for_cql_and_get_hosts(cql: Session, servers: list[ServerInfo], de
 
     # Take only hosts from `ip_set` (there may be more)
     hosts = [h for h in hosts if h.address in ip_set]
+
+    # Make sure `hosts` has same order as `servers`, that is: a given index will
+    # refer to the same underlying Scylla instance in both `servers` and `hosts`.
+    servers_by_ip = {srv.rpc_address: i for i, srv in enumerate(servers)}
+    hosts.sort(key=lambda x: servers_by_ip[x.address])
+
     await asyncio.gather(*(wait_for_cql(cql, h, deadline) for h in hosts))
 
     return hosts