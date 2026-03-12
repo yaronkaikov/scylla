scylladb/scylladb

transport: add basic CQL request forwarding
Add the infrastructure for forwarding CQL requests to other nodes.
When a process() call results in a node bounce (as opposed to a shard
bounce), the coordinator serializes the request and sends it via the
FORWARD_CQL_EXECUTE RPC verb to the target node.

In this patch we omit several features that allow handling more
scenarios that can happen when trying to forward a CQL request,
but the RPC request and response are already prepared for them.
They will be handled in the following commits.

message/messaging_service.cc
@@ -56,6 +56,7 @@
 #include "service/topology_guard.hh"
 #include "service/raft/join_node.hh"
 #include "db/view/view_building_state.hh"
+#include "transport/server.hh"
 #include "idl/consistency_level.dist.hh"
 #include "idl/tracing.dist.hh"
 #include "idl/result.dist.hh"
@@ -134,6 +135,7 @@
 #include "idl/storage_service.dist.impl.hh"
 #include "idl/join_node.dist.impl.hh"
 #include "idl/tasks.dist.impl.hh"
+#include "idl/forward_cql.dist.impl.hh"
 #include "gms/feature_service.hh"
 
 namespace netw {
@@ -748,6 +750,7 @@ static constexpr unsigned do_get_rpc_client_idx(messaging_verb verb) {
     case messaging_verb::PAXOS_ACCEPT:
     case messaging_verb::PAXOS_LEARN:
     case messaging_verb::PAXOS_PRUNE:
+    case messaging_verb::FORWARD_CQL_EXECUTE:
         return 2;
     case messaging_verb::MUTATION_DONE:
     case messaging_verb::MUTATION_FAILED:

yaronkaikov/scylla

message/messaging_service.cc
@@ -56,6 +56,7 @@
 #include "service/topology_guard.hh"
 #include "service/raft/join_node.hh"
 #include "db/view/view_building_state.hh"
+#include "transport/forward.hh"
 #include "idl/consistency_level.dist.hh"
 #include "idl/tracing.dist.hh"
 #include "idl/result.dist.hh"
@@ -136,6 +137,7 @@
 #include "idl/storage_service.dist.impl.hh"
 #include "idl/join_node.dist.impl.hh"
 #include "idl/tasks.dist.impl.hh"
+#include "idl/forward_cql.dist.impl.hh"
 #include "gms/feature_service.hh"
 
 namespace netw {
@@ -752,6 +754,7 @@ static constexpr unsigned do_get_rpc_client_idx(messaging_verb verb) {
     case messaging_verb::PAXOS_ACCEPT:
     case messaging_verb::PAXOS_LEARN:
     case messaging_verb::PAXOS_PRUNE:
+    case messaging_verb::FORWARD_CQL_EXECUTE:
         return 2;
     case messaging_verb::MUTATION_DONE:
     case messaging_verb::MUTATION_FAILED: